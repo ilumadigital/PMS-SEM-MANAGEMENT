30b4c3d608aa274f5c4b897abb2486e1d8d3f2d0
Ioannis Mavrodimos <giannis@iluma.gr>
2026-09-03 14:15:04 +0300

    Do not override Cloudbeds app permission scopes

--- backend/services/cloudbeds.service.js
+++ backend/services/cloudbeds.service.js
@@ -725,7 +725,6 @@ function buildAuthorizationUrl() {
     url.searchParams.set('client_id', clientId);
     url.searchParams.set('redirect_uri', REDIRECT_URI);
     url.searchParams.set('response_type', 'code');
-    url.searchParams.set('scope', '');
     return url.toString();
 }
 
