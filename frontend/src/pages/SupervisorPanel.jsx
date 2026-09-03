df4b412e71295b5bff11dd0a31487ff80cfab4d3
Ioannis Mavrodimos <giannis@iluma.gr>
2026-09-03 13:48:12 +0300

    Remove supervisor demo reservation dependencies

--- frontend/src/pages/SupervisorPanel.jsx
+++ frontend/src/pages/SupervisorPanel.jsx
@@ -1,19 +1,13 @@
-import React, { useMemo, useState } from 'react';
-
-import {
-  cleaningTasks,
-  properties,
-  reservations,
-  rooms,
-  shuttleRequests,
-} from '../data/semDemoData';
-
+import React, { useContext, useMemo, useState } from 'react';
 import {
   getPropertyById,
   getRoomById,
 } from '../utils/semOperationsMetrics';
 
 const SupervisorPanel = () => {
+  const { reservations, properties, rooms } = useContext(CloudbedsDataContext);
+  const cleaningTasks = [];
+  const shuttleRequests = [];
   const [tasks, setTasks] = useState(cleaningTasks);
   const [activeFilter, setActiveFilter] = useState('all');
 
